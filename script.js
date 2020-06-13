09d8c7d5dc57af76baeb0b69d623d9465050b72f
Glyn Davies <glynwdavies@gmail.com>
2020-06-13 15:01:52 +0100

    Scripting completed

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
+const errorMessage = document.querySelector('.error-message');
 
 const apiURL = 'https://api.lyrics.ovh';
 
 async function searchSongs(term) {
+    // ---------------------------------------
+    // Using fetch
+    // ---------------------------------------
+    // fetch(`${apiURL}/suggest/${term}`)
+    //     .then(response => response.json())
+    //     .then(data => showData(data));
+    // ---------------------------------------
+    // Using async/await
+    // ---------------------------------------
     const response = await fetch(`${apiURL}/suggest/${term}`);
     const data = await response.json();
     showData(data);
@@ -18,7 +28,7 @@ function showData(data) {
                 <li>
                     <span>
                         <strong>${song.artist.name}</strong>
-                        &nbsp;-&nbsp;${song.title}
+                        &nbsp;&#45;&nbsp;${song.title}
                     </span>
                     <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">
                         Get Lyrics
@@ -26,6 +36,23 @@ function showData(data) {
                 </li>`).join('')}
         </ul>
     `;
+    if(data.prev || data.next) {
+        more.innerHTML = `
+            ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Previous</button>` : ''}
+            ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+        `;
+    } else {
+        more.innerHTML = '';
+    }
+}
+
+async function getMoreSongs(url) {
+    // ------------------------------------------------------------------------
+    // Pass the request through a proxy to bypass CORS
+    // ------------------------------------------------------------------------
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await response.json();
+    showData(data);
 }
 
 async function getLyrics(artist, songTitle) {
@@ -38,13 +65,20 @@ async function getLyrics(artist, songTitle) {
         </h2>
         <span>${lyrics}</span>
     `;
+    more.innerHTML = '';
 }
 
 form.addEventListener('submit', event => {
     event.preventDefault();
     const searchTerm = search.value.trim();
     if(!searchTerm) {
-        alert('Please type in a search term!');
+        // alert('Please type in a search term!');
+        errorMessage.classList.remove('invisible');
+        errorMessage.classList.add('visible');
+        setTimeout(() => {
+            errorMessage.classList.remove('visible');
+            errorMessage.classList.add('invisible');
+        },1000);
     } else {
         searchSongs(searchTerm);
     }
@@ -53,6 +87,9 @@ form.addEventListener('submit', event => {
 result.addEventListener('click', event => {
     const clickedElement = event.target;
     if(clickedElement.tagName === 'BUTTON') {
+        // -------------------------------------------------------------
+        // Use the data attributes
+        // -------------------------------------------------------------
         const artist = clickedElement.getAttribute('data-artist');
         const songTitle = clickedElement.getAttribute('data-songtitle');
         getLyrics(artist, songTitle);
