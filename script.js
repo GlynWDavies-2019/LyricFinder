39341fd2c214ae446c37383fb9fbd2b0ab63ad62
Glyn Davies <glynwdavies@gmail.com>
2020-06-13 08:41:50 +0100

    Lyric fetch logic completed

--- script.js
+++ script.js
@@ -14,8 +14,29 @@ async function searchSongs(term) {
 function showData(data) {
     result.innerHTML = `
         <ul class="songs">
-            ${data.data.map(song => `<li><span><strong>${song.artist.name}</strong>&nbsp;-&nbsp;${song.title}</span></li>`).join('')}
+            ${data.data.map(song => `
+                <li>
+                    <span>
+                        <strong>${song.artist.name}</strong>
+                        &nbsp;-&nbsp;${song.title}
+                    </span>
+                    <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">
+                        Get Lyrics
+                    </button>
+                </li>`).join('')}
         </ul>
+    `;
+}
+
+async function getLyrics(artist, songTitle) {
+    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await response.json();
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g,'<br>');
+    result.innerHTML = `
+        <h2>
+            <strong>${artist}</strong>&nbsp;&#45;&nbsp${songTitle}
+        </h2>
+        <span>${lyrics}</span>
     `;
 }
 
@@ -26,5 +47,14 @@ form.addEventListener('submit', event => {
         alert('Please type in a search term!');
     } else {
         searchSongs(searchTerm);
+    }
+});
+
+result.addEventListener('click', event => {
+    const clickedElement = event.target;
+    if(clickedElement.tagName === 'BUTTON') {
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songtitle');
+        getLyrics(artist, songTitle);
     }
 });
